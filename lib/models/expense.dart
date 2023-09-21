bdd43325257893549fba2b23ab03ccd1183b0893
Dardnos <adne.o94@gmail.com>
2023-09-21 10:41:08 +0200

    Added describing comment lines to all dart files

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,18 @@
+// Import necessary packages and libraries
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
-import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart'; // For generating unique IDs
+import 'package:intl/intl.dart'; // For date formatting
 
+// Create a date formatter with the "month/day/year" format
 final formatter = DateFormat.yMd();
 
+// Generate a UUID instance for generating unique IDs
 const uuid = Uuid();
 
+// Define an enumeration 'Category' to represent expense categories
 enum Category { food, travel, leisure, work }
 
+// Define icons for each expense category
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
@@ -15,44 +20,49 @@ const categoryIcons = {
   Category.work: Icons.work,
 };
 
+// Define the 'Expense' class to represent individual expenses
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
     required this.category,
-  }) : id = uuid.v4();
+  }) : id = uuid.v4(); // Generate a unique ID for each expense
 
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-  final Category category;
+  final String id; // Unique ID for the expense
+  final String title; // Title of the expense
+  final double amount; // Amount spent
+  final DateTime date; // Date of the expense
+  final Category category; // Category of the expense
 
+  // Get the formatted date as a string
   String get formattedDate {
     return formatter.format(date);
   }
 }
 
+// Define the 'ExpenseBucket' class to group expenses by category
 class ExpenseBucket {
   const ExpenseBucket({
     required this.category,
     required this.expenses,
   });
 
+  // Create an 'ExpenseBucket' for a specific category from a list of all expenses
   ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
       : expenses = allExpenses
-            .where((expense) => expense.category == category)
-            .toList();
+      .where((expense) => expense.category == category)
+      .toList();
 
-  final Category category;
-  final List<Expense> expenses;
+  final Category category; // Category of the expenses in this bucket
+  final List<Expense> expenses; // List of expenses in this category
 
+  // Calculate the total expenses within this bucket
   double get totalExpenses {
     double sum = 0;
 
     for (final expense in expenses) {
-      sum += expense.amount; // sum = sum + expense.amount
+      sum += expense.amount; // Calculate the sum of amounts
     }
 
     return sum;
